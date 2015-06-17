ae04e40ec1819e81ccf0254fb8fc1d8a714e2c91
yiendos <yiendos@sbmatrix.com>
2015-06-17 13:05:12 +0100

    re initial commit

---
+++ gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+    // load time-grunt and all grunt plugins found in the package.json
+    require('jit-grunt')(grunt);
+
+    // grunt config
+    grunt.initConfig({
+
+        // Sass
+        sass: {
+            options: {
+                'outputStyle': 'compressed'
+            },
+            main: {
+                files: [{
+                    '_jekyll/css/style.css': '_scss/style.scss'
+                }]
+            }
+        },
+
+        // Browser Sync
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : ["_site/*.*"]
+                },
+                options: {
+                    port: 7669, // snow on phone keypad
+                    open: true, // Opens site in your default browser, no need to remember the port
+                    notify: false,
+                    watchTask: true,
+                    injectChanges: false,
+                    server: {
+                        baseDir: '_site'
+                    }
+                }
+            }
+        },
+
+        // Shell commands
+        shell: {
+            jekyllBuild: {
+                command: 'bundle exec jekyll build --config _config.yml,_config.dev.yml'
+            }
+        },
+
+
+        // Watch files
+        watch: {
+            sass: {
+                files: [
+                    // Including
+                    '_scss/style.scss',
+                    '_scss/**/*.scss'
+                ],
+                tasks: ['sass:main', 'autoprefixer'],
+                options: {
+                    interrupt: true,
+                    atBegin: true
+                }
+            },
+            jekyll: {
+                files: [
+                    '_config.yml',
+                    '_config.dev.yml'
+                ],
+                tasks: ['shell:jekyllBuild'],
+                options: {
+                    interrupt: false,
+                    atBegin: true
+                }
+            }
+        }
+    });
+
+    // The dev task will be used during development
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
